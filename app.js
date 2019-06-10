0b0dab3c2f0cc2a5d41a7558a1aa5c5bfc211f43
Svon <svon.me@gmail.com>
2019-06-10 16:01:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@
          headers: {
              'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
          },
+         withCredentials: true,
          transformRequest: [
              function(data) {
                  if (data) {
